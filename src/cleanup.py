494e758cd37f8791e479e8e316807136389a2601
dbanerjee1410 <40912684+dbanerjee1410@users.noreply.github.com>
2022-03-02 01:06:27 +0530

    Change in filepath

--- src/cleanup.py
+++ src/cleanup.py
@@ -7,6 +7,6 @@ from constant import Constant
 
 jsonfile = util.get_json_from_log(Constant.LOGFILE_DIR + "\startup.log")
 
-dbfile = {"date": util.get_curr_date_str(), "LogfileName": "Startup.log", "logfilePath": Constant.LOGFILE_DIR + "\startup.log",
+dbfile = {"date": util.get_curr_date_str(), "LogfileName": "Startup.log", "logfilePath": Constant.LOGFILE_DIR,
           "timestamp": util.get_epoch(), "file": jsonfile}
 util.insert_log_to_mango(dbfile)
