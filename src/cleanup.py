a4fcd28f6720ee91ca638177ced5a09d6a52e472
dbanerjee1410 <40912684+dbanerjee1410@users.noreply.github.com>
2022-03-03 00:11:09 +0530

    Cleanup file path correction

--- src/cleanup.py
+++ src/cleanup.py
@@ -5,7 +5,7 @@ import util
 from constant import Constant
 
 
-jsonfile = util.get_json_from_log(Constant.LOGFILE_DIR + "\startup.log")
+jsonfile = util.get_json_from_log(Constant.LOGFILE_DIR + "/startup.log")
 
 dbfile = {"date": util.get_curr_date_str(), "LogfileName": "Startup.log", "logfilePath": Constant.LOGFILE_DIR,
           "timestamp": util.get_epoch(), "file": jsonfile}
